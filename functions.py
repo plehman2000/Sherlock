e10ea3623cc18ef788689ff8811c568ff0282510
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2022-01-29 19:12:10 -0500

    Combined functions

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import spacy
 import requests
 import spacy
 import openai
+from fuzzywuzzy import process
+
 from youtube_transcript_api import YouTubeTranscriptApi
 
 
@@ -11,6 +13,60 @@ nlp = spacy.load('en_core_web_lg')
 
 openai.organization = "org-xYSZAaZqytg7EQ0juDkS4z5D"
 openai.api_key = "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"
+
+
+
+def getEntTsPairs(entities_from_transcript):
+    """
+    Returns a dictionary with key = entity name and value = list of timestamps assocaited with entity
+    Takes input from the result of getTranscriptFromURL
+    """
+
+
+    ent_ts_pairs = dict()
+    for ent_list in entities_from_transcript:
+        entity = ent_list[0][0][0]
+        timestamp = ent_list[1]
+        
+        if entity not in ent_ts_pairs.keys():
+            ent_ts_pairs[entity] = []
+        ent_ts_pairs[entity].append(timestamp)
+    return ent_ts_pairs
+            
+def fuzzySearch(query, ent_ts_pairs, limit=-1):
+    """
+    Returns a dictionary with key = query word and value = list of lists (kw_rel_ts_list).
+
+    kw_rel_ts_list has 3 values:
+        kw_rel_ts[0] = entity name
+        kw_rel_ts[1] = relevance relative to query of entity
+        kw_rel_ts[2] = list of timestamps where query appears in video
+    """
+
+    entities_list = ent_ts_pairs.keys()
+    process_limit = len(entities_list)
+    if limit != -1:
+        assert limit > 0 and type(limit) is int, "Limit should be an integer greater than 0."
+        process_limit = limit
+        
+    keywords = query.split(";")
+    sorted_dict = {}
+
+    for word in keywords:
+        sorted_dict[word] = process.extract(word, entities_list, limit=process_limit)
+        
+    kw_rel_ts_sorted_dict = dict()
+    for kw, kw_relevance_tuples_list in sorted_dict.items():
+        kw_rel_ts_list = []
+        for kw_relevance_tuple in kw_relevance_tuples_list:
+            curr_entity = kw_relevance_tuple[0]
+            kw_rel_ts = [curr_entity, kw_relevance_tuple[1], ent_ts_pairs[curr_entity]]
+            kw_rel_ts_list.append(kw_rel_ts)
+    kw_rel_ts_sorted_dict[kw] = kw_rel_ts_list
+    
+    return kw_rel_ts_sorted_dict
+
+
 
 def get_ents(text):
     #show entities in text
